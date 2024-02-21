fc900162429a668b3beea4c6d72d7993a253e928
Diêgo Ferreira <diegofrr.19@gmail.com>
2024-02-21 01:24:59 -0300

    refactor: updated certificates link

--- js/data.js
+++ js/data.js
@@ -3,32 +3,32 @@ export const certificates = [
     title: "Web Design Completo: HTML5, CSS3 e JavaScript",
     hours: "54.5h",
     academy: "Udemy",
-    link: "https://www.freecodecamp.org/certification/diegofrr/scrum",
+    link: "https://www.udemy.com/certificate/UC-bd301995-019d-4b0e-8aec-6a5c740b745d",
   },
 
   {
     title: "React JS do zero ao avançado na prática",
     hours: "22.5h",
     academy: "Udemy",
-    link: "https://www.freecodecamp.org/certification/diegofrr/scrum",
+    link: "https://www.udemy.com/certificate/UC-21926e85-fe09-4322-a48a-3a6d8646f3ab",
   },
   {
     title: "React Native: Criando aplicativos do zero",
     hours: "22h",
     academy: "Udemy",
-    link: "https://www.freecodecamp.org/certification/diegofrr/scrum",
+    link: "https://www.udemy.com/certificate/UC-24f64717-e724-42f8-beec-f9811f048464",
   },
   {
     title: "NextJS do zero ao avançado na prática",
     hours: "14.5h",
     academy: "Udemy",
-    link: "https://www.freecodecamp.org/certification/diegofrr/scrum",
+    link: "https://www.udemy.com/certificate/UC-d503a86a-15fc-4065-af3a-1d8f2a4e5bfc",
   },
   {
     title: "APIs com Nodejs, Express + MongoDB e SQL",
     hours: "6h",
     academy: "Udemy",
-    link: "https://www.freecodecamp.org/certification/diegofrr/scrum",
+    link: "https://www.udemy.com/certificate/UC-17f6770a-a8e1-4f24-893d-919f8d16ab5a/",
   },
 ];
 
